8b7f3254531724b6e2b0175cde59e504bc3226d5
Rickexotic <128307082+Rickexotic@users.noreply.github.com>
2026-09-02 16:49:48 -0400

    Add configuration constants to config.js
    Added configuration constants for tenant, client, site, incidents list, and redirect URI.

--- js/config.js
+++ js/config.js
@@ -1,1 +1,7 @@
-
+const CONFIG = {
+    tenantId: "0d1b3424-77f4-41c7-8273-1e1a1e17060d",
+    clientId: "d17656ff-7746-492d-9e5c-4644cf1f3ea5",
+    siteId: "f2fd451e-67f7-44d4-8aae-a5fcdd33dc3b",
+    incidentsListId: "f2fd451e-67f7-44d4-8aae-a5fcdd33dc3b",
+    redirectUri: "https://rickexotic.github.io"
+};
