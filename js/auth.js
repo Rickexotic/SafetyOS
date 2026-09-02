bf68664ad2e0db6900705849cc3bb593f79efe3c
Rickexotic <128307082+Rickexotic@users.noreply.github.com>
2026-09-02 16:53:26 -0400

    Implement MSAL authentication in auth.js
    Added MSAL configuration and sign-in function for authentication.

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,48 @@
+const msalConfig = {
+
+    auth: {
+
+        clientId: CONFIG.clientId,
+
+        authority:
+            `https://login.microsoftonline.com/${CONFIG.tenantId}`,
+
+        redirectUri:
+            CONFIG.redirectUri
+    }
+};
+
+const msalInstance =
+    new msal.PublicClientApplication(msalConfig);
+
+let activeAccount = null;
+
+async function signIn(){
+
+    try{
+
+        const loginResponse =
+            await msalInstance.loginPopup({
+
+                scopes:[
+                    "User.Read",
+                    "Sites.ReadWrite.All"
+                ]
+            });
+
+        activeAccount =
+            loginResponse.account;
+
+        document.getElementById("userInfo")
+            .innerText =
+            activeAccount.username;
+
+    }
+    catch(error){
+
+        console.error(error);
+
+        alert("Login failed");
+    }
+}
 
