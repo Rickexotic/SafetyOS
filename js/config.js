6da3bfcc972d2a3dab8a09eb4806df34f2dc1081
Rickexotic <128307082+Rickexotic@users.noreply.github.com>
2026-09-03 18:25:35 -0400

    Fix syntax error in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
     tenantId: "0d1b3424-77f4-41c7-8273-1e1a1e17060d",
     clientId: "d17656ff-7746-492d-9e5c-4644cf1f3ea5",
-
     siteId: "b534b512-fbbf-4ab3-892f-5ca122ef9c17",
-
     incidentsListId: "63fe38d9-e399-4599-8325-53dca3f18604",
-
     redirectUri: "https://rickexotic.github.io/SafetyOS"
 };
